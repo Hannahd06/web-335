f5f481079203beb40c4156bf70b355b7448f3c07
Hannah Del Real <hdelreal@my365.bellevue.edu>
2023-06-01 18:12:57 -0700

    adding files for assignment 3.3

---
+++ week-2/delreal_myworld.py
@@ -0,0 +1,11 @@
+""" 
+    Title: delreal_myworld.py
+    Author: Hannah Del Real
+    Date: 1 June 2023
+    Description: First Python Assignment 2.3  "You are in my world!"
+"""
+
+
+
+# print message to terminal 
+print("You are now in Del Real's world!")
