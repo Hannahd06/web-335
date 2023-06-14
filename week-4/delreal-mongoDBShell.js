1b212cb5b5ab48afc988df11717bb9f563c667cb
Hannah Del Real <hdelreal@my365.bellevue.edu>
2023-06-14 16:41:54 -0700

    adding assigment for week-4

---
+++ week-4/delreal-mongoDBShell.js
@@ -0,0 +1,21 @@
+/**
+	Title: delreal-assignment4.2-mongoDB-shell.js
+    Author: Professor Krasso
+    Date: 13 June 2023
+    Description: MongoDB Shell Scripts for the users collection.
+ */
+
+// Query to display all documents in users collection
+db.users.find();
+
+// Query to find user with email address jbach@me.com 
+db.users.find({email: "jbach@me.com"});
+
+// Query to find user with lastName Mozart
+db.users.find({lastName: "Mozart"});
+
+// Query to find user with firstName Richard
+db.users.find({firstName: "Richard"});
+
+// Query to find user with employeeId 1010 
+db.users.find({employeeId: "1010"}); 
