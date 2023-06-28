21679124d4bcdc9fa02c92b38cbc19e6822789e2
Hannah Del Real <hdelreal@my365.bellevue.edu>
2023-06-28 16:35:43 -0700

    adding comments

--- week-6/delreal_usersp1.py
+++ week-6/delreal_usersp1.py
@@ -1,3 +1,9 @@
+""" 
+    Title: delreal_usersp1.py
+    Author: Hannah Del Real
+    Date: 27 June 2023
+    Description: Connecting Python with MongoDB
+"""
 # Import mongoDBClient
 from pymongo import MongoClient
 # Create a connection string to connect to
@@ -9,14 +15,17 @@ print(client)
 db = client['web335DB']
 
 print("Listing of all users in the web335 collection")
+
 # Call the find function to display all users in the collection, displaying only firstName and LastName
 for user in db.users.find({}, {"firstName": 1, "lastName": 1}):
     print(user)
 
 print("Displaying information for employeeId: 1011")
+
 # Call the find_one function to display a document with employeeId: 1011
 print(db.users.find_one({"employeeId": "1011"}))
 
 print("Displaying information for lastName: Mozart")
+
 # Call the find_one function to display a document with the lastName Mozart
 print(db.users.find_one({"lastName": "Mozart"}))
